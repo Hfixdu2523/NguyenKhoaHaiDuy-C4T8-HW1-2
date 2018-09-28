73391703f4c95b8331d592ce8c6303c575ba5caf
Hfixdu2523 <nguyenkhoahaiduy@gmail.com>
2018-09-28 19:17:09 +0700

    OK

--- session4/turtle_intro.py
+++ session4/turtle_intro.py
@@ -2,12 +2,11 @@ from turtle import*
 colormode(255)
 speed(0)
 for i in range(30):
-    color(216, 111, 126)
+    color(216, i * 30, 126)
     for j in range(4):
         forward(66)
         left(90)
-        forward(50)
-        right(27)
+        
 
     left(23)    
 mainloop()
