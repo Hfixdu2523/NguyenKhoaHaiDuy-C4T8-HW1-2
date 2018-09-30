1fe684707ec91dc54496f03b27b14b87db3a54c4
Hfixdu2523 <nguyenkhoahaiduy@gmail.com>
2018-09-30 08:18:24 +0700

    session6

--- session6/B4.py
+++ session6/B4.py
@@ -2,6 +2,9 @@ while True:
     string = input("Enter username: ")
     if string.isalpha():
         print("Invalid username")
+    elif string.isnumeric():
+        print("Valid username")
+        break
     elif string.isalnum():
         print("Valid username")
         break
