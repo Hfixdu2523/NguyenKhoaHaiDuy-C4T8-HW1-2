4e3832228c4efb3000324529c5d851e0a6f8838e
Hfixdu2523 <nguyenkhoahaiduy@gmail.com>
2018-11-02 21:10:46 +0700

    web design

--- session12/b2.py
+++ session12/b2.py
@@ -19,20 +19,20 @@
 #     r = ydl.extract_info('https://www.youtube.com/watch?v=Wv2rLZmbPMA', download=False)
 #     print(r)
 
-from __future__ import unicode_literals               # f)
-import youtube_dl
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    URL = ['https://www.youtube.com/watch?v=Wv2rLZmbPMA', 'https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s', 'https://www.youtube.com/watch?v=GIDoQsQyS0s']
-    for i in URL:
-        ext = ydl.extract_info(i, download = False)
-    exted = [ext]
-    print(exted)
+# from __future__ import unicode_literals               # f) + h)
+# import youtube_dl
+# ydl_opts = {}
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#     URL = ['https://www.youtube.com/watch?v=Wv2rLZmbPMA', 'https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s', 'https://www.youtube.com/watch?v=GIDoQsQyS0s']
+#     for i in URL:
+#         ext = ydl.extract_info(i, download = False)
+#     exted = [ext]
+#     print(exted)
 
-import json
-var = exted
-with open('data1.json', 'w') as outfile:
-    json.dump(var, outfile)
+# import json
+# var = exted
+# with open('data1.json', 'w') as outfile:
+#     json.dump(var, outfile)
 
 
 # var = 'I love manga and anime'                        # g)
